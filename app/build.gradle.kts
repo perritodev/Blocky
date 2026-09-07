3dc9637a9f08001bec31cee6479500b8c7fd90b6
perritodev <elperroesnavi@gmail.com>
2026-09-07 01:07:23 -0600

    chore: bump version to 1.0.17 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.omargarcia.Blocky"
         minSdk = 29
         targetSdk = 37
-        versionCode = 18
-        versionName = "1.0.16"
+        versionCode = 19
+        versionName = "1.0.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
